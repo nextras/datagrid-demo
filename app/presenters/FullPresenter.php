2c4bea8ac633dd47e40c6e82f5dc7b9fbb55b583
Jan Skrasek <hrach.cz@gmail.com>
2017-02-11 15:41:08 +0100

    full: reworked default filtering

--- app/presenters/FullPresenter.php
+++ app/presenters/FullPresenter.php
@@ -10,7 +10,7 @@ final class FullPresenter extends BasePresenter
 {
 	public function createComponentDatagrid()
 	{
-		$grid = new Datagrid;
+		$grid = new Datagrid();
 		$grid->addColumn('id');
 		$grid->addColumn('firstname')->enableSort($grid::ORDER_ASC);
 		$grid->addColumn('surname')->enableSort();
@@ -21,19 +21,19 @@ final class FullPresenter extends BasePresenter
 		$grid->setDataSourceCallback([$this, 'getDataSource']);
 		$grid->setPagination(10, [$this, 'getDataSourceSum']);
 		$grid->setFilterFormFactory(function() {
-			$form = new Container;
-			$form->addText('firstname');
+			$form = new Container();
+			$form->addText('firstname')->setDefaultValue('Ed');
 			$form->addText('surname');
-			$form->addSelect('gender', NULL, array(
+			$form->addSelect('gender', null, [
 				'male' => 'male',
 				'female' => 'female',
-			))->setPrompt('---')->setDefaultValue('male');
+			])->setPrompt('---');
 
 			return $form;
 		});
 
 		$grid->setEditFormFactory(function($row) {
-			$form = new Container;
+			$form = new Container();
 			$form->addText('firstname')->setRequired('You have to enter the first name');
 			$form->addText('surname')->setRequired('You have to enter the last name.');
 			!$row ?: $form->setDefaults($row);
@@ -53,8 +53,5 @@ final class FullPresenter extends BasePresenter
 		$values = $form->getValues();
 		$user = $this->context->table('user')->get($values->id);
 		$user->update($values);
-
-		$this->flashMessage('Saving data: ' . json_encode($form->getValues()));
-		$this->redrawControl('flashes');
 	}
 }

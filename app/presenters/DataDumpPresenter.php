7fffbbd828267922cd474c6eced94966909cd39a
Jan Skrasek <hrach.cz@gmail.com>
2017-02-11 13:40:17 +0100

    added view action for data-dump

--- app/presenters/DataDumpPresenter.php
+++ app/presenters/DataDumpPresenter.php
@@ -17,6 +17,7 @@ final class DataDumpPresenter extends BasePresenter
 
 		$grid->setDataSourceCallback([$this, 'getDataSource']);
 		$grid->addCellsTemplate(__DIR__ . '/../../vendor/nextras/datagrid/bootstrap-style/@bootstrap3.datagrid.latte');
+		$grid->addCellsTemplate(__DIR__ . '/../templates/DataDump/@cells.latte');
 		return $grid;
 	}
 }
